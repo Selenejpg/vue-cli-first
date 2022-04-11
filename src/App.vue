a5cdecb860418470c73421839c7ceaf09484b27c
noemipintaldi <noemipintaldi@gmail.com>
2022-04-11 15:22:21 +0200

    esercizio completatato primo push

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Titolo></Titolo>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Titolo from './components/Titolo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Titolo
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Titolo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <h2></h2>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'Titolo',
+}
+</script>
+
+<style scoped>
+h2{
+    color: green;
+}
+</style>
